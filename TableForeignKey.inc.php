d777518f8a35ff1e55e9c254254bf319d2b77efc
alcexhim <alcexhim@gmail.com>
2015-01-13 13:42:03 -0500

    Updated comments

--- TableForeignKey.inc.php
+++ TableForeignKey.inc.php
@@ -2,13 +2,38 @@
 	namespace DataFX;
 	
 	\Enum::Create("DataFX\\TableForeignKeyReferenceOption", "Restrict", "Cascade", "SetNull", "NoAction");
+	
+	/**
+	 * Defines a relationship to a column in another table.
+	 * @author Michael Becker
+	 */
 	class TableForeignKey
 	{
+		/**
+		 * The ID of the foreign key. Can be null. 
+		 * @var string
+		 */
 		public $ID;
+		/**
+		 * The name of the column in the source table.
+		 * @var string
+		 */
 		public $ColumnName;
+		/**
+		 * The reference(s) to the foreign column(s) in the foreign table(s).
+		 * @var TableForeignKeyColumn[]
+		 */
 		public $ForeignColumnReference;
 		
+		/**
+		 * The action to take when a value in this column is deleted.
+		 * @var TableForeignKeyReferenceOption
+		 */
 		public $DeleteAction;
+		/**
+		 * The action to take when a value in this column is updated.
+		 * @var TableForeignKeyReferenceOption
+		 */
 		public $UpdateAction;
 		
 		public function __construct($columnName, $foreignColumnReference, $deleteAction = null, $updateAction = null, $id = null)
@@ -25,9 +50,22 @@
 	}
 	class TableForeignKeyColumn
 	{
+		/**
+		 * The table that contains the foreign column.
+		 * @var Table
+		 */
 		public $Table;
+		/**
+		 * A reference to the foreign column, or the name of the foreign column.
+		 * @var string|Column
+		 */
 		public $Column;
 		
+		/**
+		 * Creates a TableForeignKeyColumn with the given parameters.
+		 * @param Table $table The table that contains the foreign column.
+		 * @param string|Column $column A reference to the foreign column, or the name of the foreign column.
+		 */
 		public function __construct($table, $column)
 		{
 			$this->Table = $table;
